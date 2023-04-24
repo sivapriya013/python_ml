3d0fc194e0d37127be5e508f99ffb65b8a1d7cb3
sivapriya013 <sivapriyashaji99@gmail.com>
2023-04-24 17:07:46 +0530

    implimented quadratic program

---
+++ quadratic.py
@@ -0,0 +1,9 @@
+a = int(input("value of a:"))
+b = int(input("value of b:"))
+c = int(input("value of c:"))
+k = (b**2)-(4*a*c)
+d = k**0.5
+x1 = (-b+d)/(2*a)
+x2 = (-b-d)/(2*a)
+print("The roots are ", x1 ,x2 )
+
