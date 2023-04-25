0d2f7f24a256673d806fbeeb6b5ec7d9df5613b6
sivapriya013 <sivapriyashaji99@gmail.com>
2023-04-25 17:38:40 +0530

    added upper lower program

---
+++ qus.py
@@ -0,0 +1,31 @@
+# x = input("name:")
+# k = ""
+# for i in x:
+#
+#     if i. islower():
+#         k+= i.upper()
+#
+#     else :
+#         k+=i.lower()
+#
+# print(k)
+#
+#
+def swape(x):
+    k = ""
+
+    for i in x:
+        if i. islower():
+            k += i.upper()
+
+        else :
+            k += i.lower()
+    return(k)
+
+name = input("name:")
+y = swape(name)
+print(y)
+
+
+
+
