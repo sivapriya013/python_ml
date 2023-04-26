a7aee1be09b5ab00dbff469325eb9ca3e28d938b
sivapriya013 <sivapriyashaji99@gmail.com>
2023-04-26 13:57:42 +0530

    to check whether its a leap year or not

---
+++ loops.py
@@ -0,0 +1,25 @@
+# for i in range(1,5):
+#     for j in range(1,4):
+#         print(1,end="")
+#     print()
+
+
+# for i in range(1,5):
+#     for j in range(1,4):
+#         print("*",end="")
+#     print()
+
+# for i in range(1,5):
+#     for j in range(1,4):
+#         print(i,end="")
+#     print()
+
+# for i in range(1,5):
+#     for j in range(1,4):
+#         print(j,end="")
+#     print()
+
+for i in range(1,5):
+    for j in range(1,4):
+        print(j+4,end="")
+    print()
